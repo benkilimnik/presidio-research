83b27ab1f25d025eb4b7fe1e14270059e69df85c
Benjamin Kilimnik <benjamin_kilimnik@brown.edu>
2022-09-20 12:01:57 -0400

    update add_unk flair

--- presidio_evaluator/models/flair_train.py
+++ presidio_evaluator/models/flair_train.py
@@ -125,12 +125,12 @@ class FlairTrainer:
         tag_type = "ner"
 
         # 3. make the tag dictionary from the corpus
-        tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
-        # if add_unk:
-        #     tag_dictionary = corpus.make_label_dictionary(label_type=tag_type)
-        # else:
-        #     tag_dictionary = corpus.make_label_dictionary(
-        #         label_type=tag_type, add_unk=False)
+        # tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
+        if add_unk:
+            tag_dictionary = corpus.make_label_dictionary(label_type=tag_type)
+        else:
+            tag_dictionary = corpus.make_label_dictionary(
+                label_type=tag_type, add_unk=False)
         print("Tag dictionary: ", tag_dictionary)
 
         # 4. initialize embeddings
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from os import path
 
 setup(
     name="presidio-evaluator",
-    version="0.0.26",
+    version="0.0.27",
     packages=find_packages(exclude=["tests"]),
     url="https://www.github.com/microsoft/presidio-research",
     license="MIT",
