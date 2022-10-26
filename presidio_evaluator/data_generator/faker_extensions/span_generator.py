230ec98d4879679bb008313eec00f1f5f14b8329
Benjamin Kilimnik <benjamin_kilimnik@brown.edu>
2022-10-26 00:56:11 -0400

    update span faker

--- presidio_evaluator/data_generator/faker_extensions/span_generator.py
+++ presidio_evaluator/data_generator/faker_extensions/span_generator.py
@@ -1,7 +1,8 @@
 import re
 from typing import List, Union, Optional
 
-from faker import Generator
+from faker import Generator, Faker
+from typing import Tuple
 
 from presidio_evaluator.data_generator.faker_extensions import (
     FakerSpansResult,
@@ -11,7 +12,7 @@ from presidio_evaluator.data_generator.faker_extensions import (
 _re_token = re.compile(r"\{\{\s*(\w+)(:\s*\w+?)?\s*\}\}")
 
 
-class SpanGenerator(Faker):
+class SpanGenerator(Generator):
     """Generator which also returns the indices of fake values.
 
     :example:
@@ -29,9 +30,6 @@ class SpanGenerator(Faker):
     >>>str(res)
         "My child's name is Daniel Gallagher"
     """
-    def __init__(self, locale="en_US", **kwargs):
-        # call init of Faker, passing given locale
-        super().__init__(locale=locale, **kwargs)
 
     def parse(
         self, text: str, add_spans: bool = False, template_id: Optional[int] = None
@@ -102,3 +100,53 @@ class SpanGenerator(Faker):
             )
 
         return results
+
+
+class SpanFaker(Faker):
+    
+    def __init__(self, locale="en_US", **kwargs):
+        # call init of Faker, passing given locale
+        super().__init__(locale=locale, **kwargs)
+
+    def parse(self, template: str, template_id: int) -> FakerSpansResult:
+        """Parse a payload template into a token-wise labeled span."""
+        spans = []
+        for match in _re_token.finditer(template):
+            faker_attribute = match.group()[2:-2]
+            spans.append(
+                FakerSpan(
+                    type=faker_attribute,
+                    start=match.start(),
+                    end=match.end(),
+                    # format calls the Faker generator, producing a (non-)PII value
+                    value=str(self.format(faker_attribute.strip())),
+                )
+            )
+        spans.sort(reverse=True, key=lambda x: x.start)
+        prev_end, payload_string = self.update_indices(spans, template)
+        payload_string = f"{template[0:prev_end]}{payload_string}"
+        return (
+            FakerSpansResult(
+                fake=payload_string, spans=spans, template=template, template_id=template_id
+            )
+        )
+
+    def update_indices(self, spans: List[FakerSpan], template: str) -> Tuple[int, str]:
+        """Update span offsets given newly generated fake (non-PII) value which was
+        inserted into the template"""
+        payload_string = ""
+        prev_end = len(template)
+        for i, span in enumerate(spans):
+            faker_attribute = span.type
+            prev_len = len(faker_attribute) + 4
+            new_len = len(f"{span.value}")
+            payload_string = f"{template[span.end: prev_end]}{payload_string}"
+            payload_string = f"{span.value}{payload_string}"
+            prev_end = span.start
+            len_difference = new_len - prev_len
+            span.end += len_difference
+            for j in range(0, i):
+                spans[j].start += len_difference
+                spans[j].end += len_difference
+            span.type = faker_attribute.strip()
+        return (prev_end, payload_string)
